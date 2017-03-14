48c2c75abeb7381685e278205d45e259132c2cc0
Guillaume Renaud <guillaume.renaud@polymtl.ca>
2017-03-14 02:57:23 -0400

    codage de la classe MoteurA et implementation dans tp8.cpp pour test

--- branche-89/tp/tp8/lib_dir/moteurA.cpp
+++ branche-89/tp/tp8/lib_dir/moteurA.cpp
@@ -4,14 +4,25 @@ MoteurA::MoteurA(void)
 {
 	TCCR2A = 0xa1; // TOP 0xff, phase correct, OC2A et OC2B pins clr upcount
 	TCCR2B = 0x02; // no noise canceler, TOP 0xff, phase correct, clk/8;
-	DDRD = 0xff; // PORT D en mode sortie
+	DDRD = 0b11111111; // PORT D en mode sortie
+	PORTD = 0b00000000; // initialisation PORT D
 }
 MoteurA::~MoteurA(void)
 {
 	
 }
-void Moteur::fwd(uint8_t speed)
+void MoteurA::fwd(uint8_t speed)
 {
-	PD5 = 0b0;
+	if(PORTD & 0b00100000) // si la direction est rev...
+		PORTD ^= 0b00100000; // ...la changer pour fwd (met un 0 a la broche 6)
 	OCR2A = speed;
+}
+void MoteurA::rev(uint8_t speed)
+{
+	PORTD = 0b00100000; // change la direction pour marche arriere
+	OCR2A = speed;
+}
+void MoteurA::stop(void)
+{
+	OCR2A = 0x00; // pwm au OC2A au ground, moteur arrete
 }
